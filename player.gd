45237f9581d4f8c88e1690387e42da2e195cba6d
Malcolm <malcolm@malcolm-Hercules-A>
2025-12-07 08:42:23 -0700

    Demo: Adding a little timer

--- player.gd
+++ player.gd
@@ -1,15 +1,14 @@
 extends Node3D
 
-
+var timer: float = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Player loaded!")
 	print("Asset:")
-	print(1)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous   frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		print("spacebar was pressed")
-	 
+	timer = timer + delta
+	print(timer)
