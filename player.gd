0664d70256f780456323683b9c14182d0e668993
Malcolm <malcolm@malcolm-Hercules-A>
2025-12-07 08:58:23 -0700

    Demo: Moving player mesh using keyboard input - Also added mesh, environment, light, and camera to player "scene"

--- player.gd
+++ player.gd
@@ -1,14 +1,12 @@
 extends Node3D
 
-var timer: float = 0.0
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Player loaded!")
-	print("Asset:")
-
 # Called every frame. 'delta' is the elapsed time since the previous   frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		print("spacebar was pressed")
-	timer = timer + delta
-	print(timer)
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta  
+		# print("spacebar was pressed")
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
+	 
