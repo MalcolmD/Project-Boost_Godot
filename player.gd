b86e0bcb15e03671b52880ee6f3702cff594c553
Malcolm <malcolm@malcolm-Hercules-A>
2025-12-27 18:02:25 -0700

    Define custom input actions and map the control keys to them. - Added input actions ["boost", "rotateLeft", "rotateRight"] and mapped directional and "wasd" keys.

--- player.gd
+++ player.gd
@@ -2,12 +2,12 @@ extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous   frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * 1000.0)  
 		# print("spacebar was pressed")
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("rotateLeft"):
 		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("rotateRight"):
 		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
 	 
    
