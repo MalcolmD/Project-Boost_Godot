86ee7ddf205824fd813597e8b1f338056814675d
Malcolm <malcolm@malcolm-Hercules-A>
2025-12-27 17:43:49 -0700

    Adding physics control to player node - Changed player node from CSGBox3D to RigidBody3D to allow physics interactions. - Added collision box (CollisionShape3D) to player object. - Added upward thrust to player object using `apply_central_force` function.

--- player.gd
+++ player.gd
@@ -1,9 +1,9 @@
-extends Node3D
+extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous   frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta  
+		apply_central_force(Vector3.UP * delta * 1000.0)  
 		# print("spacebar was pressed")
 	if Input.is_action_pressed("ui_left"):
 		rotate_z(delta)
